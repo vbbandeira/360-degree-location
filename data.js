94801de399090685ba0fc881682c11f36ce5d8bf
Vinícius Bandeira <vbritobandeira@gmail.com>
2021-05-29 14:58:14 -0300

    initial commit

---
+++ data.js
@@ -0,0 +1,60 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stand_2-1",
+      "name": "stand_2-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.7430780221007058,
+        "pitch": 0.18911156355352787,
+        "fov": 1.2311558017980793
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3755943068867342,
+          "pitch": -0.07957536474149407,
+          "rotation": 7.0685834705770345,
+          "target": "0-stand_2-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.001189580876943,
+          "pitch": -0.21296424077349485,
+          "title": "Vídeo Promocional",
+          "text": "https://www.youtube.com/channel/UCbM6llydhSDmlu1vim_P-og/featured"
+        },
+        {
+          "yaw": -0.27801567304669916,
+          "pitch": 0.03123545318055676,
+          "title": "Sobre",
+          "text": "Empresa de tecnologia 100% Palmense | Visualizador 3D | Realidade Aumentada | Spin 360 | Simulador de Ambientes 360"
+        },
+        {
+          "yaw": -1.7734742154878091,
+          "pitch": -0.5266533230353811,
+          "title": "Acesse nosso site",
+          "text": "https://www.estant.pro/"
+        }
+      ]
+    }
+  ],
+  "name": "teste-stand",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
